a467b662061bff888174771bd079c2c1180a8c38
Antonio Mika <antoniomika@gmail.com>
2016-03-16 23:36:58 -0400

    Updated stuff:

--- app/src/main/java/co/hmika/umichapi/thenewblue/DiningFragment.java
+++ app/src/main/java/co/hmika/umichapi/thenewblue/DiningFragment.java
@@ -3,9 +3,11 @@ package co.hmika.umichapi.thenewblue;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.ListFragment;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.DimenRes;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.MapFragment;
@@ -31,55 +34,99 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class DiningFragment extends Fragment {
 
-    ArrayList<String> listItems = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+    HashMap<String, HashMap<String, String>> dininghalls = new HashMap<String, HashMap<String, String>>();
+
     public MenuFragment menu = new MenuFragment();
+
+    class DiningHall {
+        private String name;
+        private String hours;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String nname) {
+            name = nname;
+        }
+
+        public String getHours() {
+            return hours;
+        }
+
+        public void setHours(String h) {
+            hours = h;
+        }
+
+        public DiningHall(String nname, String h) {
+            name = nname;
+            hours = h;
+        }
+    }
+
+    public class DiningAdapter extends ArrayAdapter<DiningHall> {
+        private ArrayList<DiningHall> items;
+        private DiningViewHolder diningHolder;
+
+        private class DiningViewHolder {
+            TextView name;
+            TextView hours;
+        }
+
+        public DiningAdapter(Context context, int tvResId, ArrayList<DiningHall> items) {
+            super(context, tvResId, items);
+            this.items = items;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater)getActivity().getSystemService(getActivity().LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.dininghall_row, null);
+                diningHolder = new DiningViewHolder();
+                diningHolder.name = (TextView)v.findViewById(R.id.dininghallname);
+                diningHolder.hours = (TextView)v.findViewById(R.id.dininghallhours);
+                v.setTag(diningHolder);
+            } else diningHolder = (DiningViewHolder)v.getTag();
+
+            DiningHall taxi = items.get(pos);
+
+            if (taxi != null) {
+                diningHolder.name.setText(taxi.getName());
+                diningHolder.hours.setText(taxi.getHours());
+            }
+
+            return v;
+        }
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.dining_fragment, container, false);
-
-        ListView lv = (ListView)v.findViewById(R.id.dining_list);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                String name = listItems.get(position);
-
-                Fragment fragment = menu;
-                Bundle args = new Bundle();
-                args.putString("name", name);
-                fragment.setArguments(args);
-
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.content_main, fragment)
-                        .commit();
-            }
-        });
-        adapter = new ArrayAdapter<String>(
-                inflater.getContext(), android.R.layout.simple_list_item_1,
-                listItems);
-        lv.setAdapter(adapter);
         new RetrieveDining().execute("https://umichapi.hmika.co/api/v1/dining");
 
         return v;
     }
 
-    public void addItems(String data) {
+    /*public void addItems(String data) {
         listItems.add(data);
         adapter.notifyDataSetChanged();
-    }
+    }*/
 
     class RetrieveDining extends AsyncTask<String, Void, String> {
 
@@ -120,11 +167,129 @@ public class DiningFragment extends Fragment {
         protected void onPostExecute(String data) {
             try {
                 JSONArray arr = new JSONArray(data);
+                final ArrayList<DiningHall> diningList = new ArrayList<DiningHall>();
 
                 for(int i = 0; i < arr.length(); i++) {
                     JSONObject hall = arr.getJSONObject(i);
-                    addItems(hall.getString("name"));
+                    if (hall.has("name") && hall.has("type")) {
+                        String name = hall.getString("name");
+                        String type = hall.getString("type");
+                        if(type.equals("B") && hall.has("hours")) {
+                            Object hours = hall.getJSONObject("hours").get("calendar_event");
+                            ArrayList<String> newtimes = new ArrayList<>();
+                            if(hours instanceof JSONObject) {
+                                JSONObject first = ((JSONObject) hours);
+
+                                if(first.has("event_time_start")) {
+                                    String feventTimeStart = first.getString("event_time_start").replace("T", " ").replace("-04:00", "");
+                                    String feventTimeEnd = first.getString("event_time_end").replace("T", " ").replace("-04:00", "");
+
+                                    SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+
+                                    Date startDate1 = dateParser.parse(feventTimeStart);
+                                    Date endDate1 = dateParser.parse(feventTimeEnd);
+
+                                    SimpleDateFormat dateFormatter1 = new SimpleDateFormat("EEEE': 'h':'mma'-'", Locale.US);
+                                    SimpleDateFormat dateFormatter2 = new SimpleDateFormat("h':'mma", Locale.US);
+                                    String date1 = dateFormatter1.format(startDate1);
+                                    String date2 = dateFormatter2.format(endDate1);
+
+                                    newtimes.add(date1 + date2);
+                                }
+                            } else if(hours instanceof JSONArray) {
+                                JSONArray times = ((JSONArray) hours);
+
+                                boolean hasDay = false;
+
+                                for(int ii = 0; ii < times.length(); ii++) {
+                                    JSONObject first = times.getJSONObject(ii);
+
+                                    if(first.has("event_time_start")) {
+                                        String feventTimeStart = first.getString("event_time_start").replace("T", " ").replace("-04:00", "");
+                                        String feventTimeEnd = first.getString("event_time_end").replace("T", " ").replace("-04:00", "");
+
+                                        SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+
+                                        Date startDate1 = dateParser.parse(feventTimeStart);
+                                        Date endDate1 = dateParser.parse(feventTimeEnd);
+
+                                        SimpleDateFormat justDate = new SimpleDateFormat("EEEE", Locale.US);
+                                        SimpleDateFormat dateFormatter1 = new SimpleDateFormat("h':'mma'-'", Locale.US);
+                                        SimpleDateFormat dateFormatter2 = new SimpleDateFormat("h':'mma", Locale.US);
+                                        String date1 = dateFormatter1.format(startDate1);
+                                        String date2 = dateFormatter2.format(endDate1);
+                                        String day = "";
+                                        if(!hasDay) {
+                                            day = justDate.format(startDate1) + ": ";
+                                            hasDay = true;
+                                        }
+                                        newtimes.add(day + date1 + date2);
+                                    }
+                                }
+                            }
+
+
+                            /*JSONObject first = times.getJSONObject(0);
+                            JSONObject scnd = times.getJSONObject(1);
+
+                            String feventTimeStart = first.getString("event_time_start").replace("T", " ").replace("-04:00", "");
+                            String feventTimeEnd = first.getString("event_time_end").replace("T", " ").replace("-04:00", "");
+
+                            String seventTimeStart = scnd.getString("event_time_start").replace("T", " ").replace("-04:00", "");
+                            String seventTimeEnd = scnd.getString("event_time_start").replace("T", " ").replace("-04:00", "");
+
+                            SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+                            Date startDate1 = dateParser.parse(feventTimeStart);
+                            Date startDate2 = dateParser.parse(seventTimeStart);
+
+                            Date endDate1 = dateParser.parse(feventTimeEnd);
+                            Date endDate2 = dateParser.parse(seventTimeEnd);
+
+                            SimpleDateFormat dateFormatter1 = new SimpleDateFormat("EEEEE', 'dd' of 'MMMMMM': 'HH':'mm'-'");
+                            SimpleDateFormat dateFormatter2 = new SimpleDateFormat("HH':'mm");
+                            String date1 = dateFormatter1.format(startDate1);
+                            String date2 = dateFormatter1.format(startDate2);
+
+                            String edate1 = dateFormatter1.format(endDate1);
+                            String edate2 = dateFormatter1.format(endDate2);*/
+
+                            //String time = date1 + edate1 + "; " + date2 + edate2;
+
+                            String listString = "";
+
+                            for (String s : newtimes) {
+                                listString += s + "; ";
+                            }
+
+                            Log.i("TNB", newtimes.toString());
+
+                            diningList.add(new DiningHall(name, listString));
+                        }
+                    }
                 }
+
+                ListView lv = (ListView)getView().findViewById(R.id.dining_list);
+
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        DiningHall hall = diningList.get(position);
+
+                        Fragment fragment = menu;
+                        Bundle args = new Bundle();
+                        args.putString("name", hall.name);
+                        fragment.setArguments(args);
+
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.content_main, fragment)
+                                .commit();
+                    }
+                });
+
+                lv.setAdapter(new DiningAdapter(getActivity(), R.layout.dininghall_row, diningList));
+
             } catch (Exception e) {
                 Log.e("TNB", e.toString());
             }
--- app/src/main/java/co/hmika/umichapi/thenewblue/MenuFragment.java
+++ app/src/main/java/co/hmika/umichapi/thenewblue/MenuFragment.java
@@ -1,11 +1,33 @@
 package co.hmika.umichapi.thenewblue;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MenuFragment extends Fragment {
 
@@ -19,8 +41,63 @@ public class MenuFragment extends Fragment {
         WebView wv = (WebView) v.findViewById(R.id.webView);
         wv.getSettings().setJavaScriptEnabled(true);
         wv.getSettings().setLoadWithOverviewMode(true);
-        wv.loadUrl("http://api.studentlife.umich.edu/menu/xml2print.php?controller=print&view=print&location=" + name);
+        wv.setWebViewClient(new CallBack());
+        wv.loadUrl("https://mobile.its.umich.edu/michigan/dining");
 
         return v;
+    }
+
+    class CallBack extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
+    };
+
+    class RetrieveDining extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                        urlConnection.disconnect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return sb.toString();
+            } catch (Exception e) {
+                Log.e("TNB", e.toString());
+                this.exception = e;
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String data) {
+            try {
+
+            } catch (Exception e) {
+                Log.e("TNB", e.toString());
+            }
+        }
     }
 }
